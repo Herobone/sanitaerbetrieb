9ca62be299602d74536d3a391f5de36436beff4a
Herobone <theherobone@gmail.com>
2021-04-09 21:28:17 +0200

    Links im footer

--- src/parts/Footer.tsx
+++ src/parts/Footer.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import classNames from "classnames";
 
@@ -9,63 +8,71 @@ import { Social } from "./Social";
 
 export const Footer = (): JSX.Element => {
     return (
-        <footer id={footerStyle.footer}>
-            <section>
-                <form method="post" action="#">
-                    <div className={mainStyle.fields}>
-                        <div className={mainStyle.field}>
-                            <label htmlFor="name">Name</label>
-                            <input type="text" name="name" id="name" />
-                        </div>
-                        <div className={mainStyle.field}>
-                            <label htmlFor="email">Email</label>
-                            <input type="text" name="email" id="email" />
-                        </div>
-                        <div className={mainStyle.field}>
-                            <label htmlFor="message">Message</label>
-                            <textarea name="message" id="message" rows={3} />
-                        </div>
-                    </div>
-                    <ul className={mainStyle.actions}>
-                        <li>
-                            <input type="submit" value="Send Message" />
-                        </li>
-                    </ul>
-                </form>
-            </section>
-            <section
-                className={classNames({
-                    [mainStyle.split]: true,
-                    [mainStyle.contact]: true,
-                })}
-            >
-                <section className={mainStyle.alt}>
-                    <h3>Address</h3>
-                    <p>
-                        1234 Somewhere Road #87257
-                        <br />
-                        Nashville, TN 00000-0000
-                    </p>
-                </section>
+        <>
+            <footer id={footerStyle.footer}>
                 <section>
-                    <h3>Phone</h3>
-                    <p>
-                        <a href="#">(000) 000-0000</a>
-                    </p>
+                    <form method="post" action="#">
+                        <div className={mainStyle.fields}>
+                            <div className={mainStyle.field}>
+                                <label htmlFor="name">Name</label>
+                                <input type="text" name="name" id="name" />
+                            </div>
+                            <div className={mainStyle.field}>
+                                <label htmlFor="email">E-Mail</label>
+                                <input type="text" name="email" id="email" />
+                            </div>
+                            <div className={mainStyle.field}>
+                                <label htmlFor="message">Nachricht</label>
+                                <textarea name="message" id="message" rows={3} />
+                            </div>
+                        </div>
+                        <ul className={mainStyle.actions}>
+                            <li>
+                                <input type="submit" value="Senden" />
+                            </li>
+                        </ul>
+                    </form>
                 </section>
-                <section>
-                    <h3>Email</h3>
-                    <p>
-                        <a href="#">info@untitled.tld</a>
-                    </p>
+                <section
+                    className={classNames({
+                        [mainStyle.split]: true,
+                        [mainStyle.contact]: true,
+                    })}
+                >
+                    <section className={mainStyle.alt}>
+                        <h3 style={{ fontSize: "2em" }}>Matthias Junge</h3>
+                    </section>
+                    <section>
+                        <h3>Telefon</h3>
+                        <p>
+                            <a href="tel:+4917632839935">0176-32839935</a>
+                        </p>
+                    </section>
+                    <section>
+                        <h3>E-Mail</h3>
+                        <p>
+                            <a href="mailto:info@sanitaerbetrieb.com">info@sanitaerbetrieb.com</a>
+                        </p>
+                    </section>
+                    <section>
+                        <h3>Sozial</h3>
+                        <Social />
+                    </section>
                 </section>
-                <section>
-                    <h3>Social</h3>
-                    <Social />
-                </section>
-            </section>
-        </footer>
+            </footer>
+            <div id={footerStyle.footerLinks}>
+                <ul>
+                    <li>
+                        <a href="/datenschutz.html">Datenschutzerklärung</a>
+                    </li>
+                    <li>
+                        <a href="/impressum.html">Impressum</a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/Herobone/sanitaerbetrieb">Quellcode</a>
+                    </li>
+                </ul>
+            </div>
+        </>
     );
 };
-
-/* eslint-enable jsx-a11y/anchor-is-valid */
